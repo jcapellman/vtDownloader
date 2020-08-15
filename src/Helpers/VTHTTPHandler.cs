714efe5ec75799b0308cd814c1bce9b50c64b039
Jarred Capellman <jcapellman@hotmail.com>
2020-08-15 17:34:56 -0500

    Change: Refactored the VTHTTPHandler class

--- src/Helpers/VTHTTPHandler.cs
+++ src/Helpers/VTHTTPHandler.cs
@@ -1,28 +1,38 @@
-﻿using System.IO;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 using VTDownloader.Enums;
+using VTDownloader.Objects;
 
 namespace VTDownloader.Helpers
 {
     public class VTHTTPHandler
     {
-        public static async Task<DownloadResponse> DownloadAsync(string downloadLocation, string vtKey, string hash)
+        public static async Task<DownloadResponseItem> DownloadAsync(string vtKey, string hash)
         {
-            using (var httpClient = new HttpClient())
+            try
             {
-                var file = await httpClient.GetByteArrayAsync(
-                    $"https://www.virustotal.com/vtapi/v2/file/download?apikey={vtKey}&hash={hash}");
-
-                if (file == null)
+                using (var httpClient = new HttpClient())
                 {
-                    return DownloadResponse.UNKNOWN_FILE;
+                    var file = await httpClient.GetByteArrayAsync(
+                        $"https://www.virustotal.com/vtapi/v2/file/download?apikey={vtKey}&hash={hash}");
+
+                    if (file == null)
+                    {
+                        return new DownloadResponseItem(DownloadResponseStatus.CANNOT_CONNECT_TO_VT);
+                    }
+
+                    return new DownloadResponseItem(file);
                 }
-
-                await File.WriteAllBytesAsync(downloadLocation, file);
-
-                return DownloadResponse.SUCCESS;
+            } catch (HttpRequestException requestException)
+            {
+                switch (requestException.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new DownloadResponseItem(DownloadResponseStatus.INVALID_VT_KEY);
+                    default:
+                        return new DownloadResponseItem(DownloadResponseStatus.UNEXPECTED_HTTP_ERROR, requestException);
+                }
             }
         }
     }
